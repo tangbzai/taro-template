a1cd5f8a665090a2a73e7e989a5d06de89ea1ae4
tangbzai <tan-jc@qq.com>
2025-10-12 22:04:57 +0800

    feat: 使用pageMap管理页面

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,21 @@
+import PAGE_MAP, { PageType, SUB_PACKAGES } from "./pageMap";
+const ALL_PAGES = Array.from(new Set(Object.values(PAGE_MAP).flat()));
+
+/** 获取页面路径 */
+function getPagesPath(pages: PageType[]): PageType["path"][] {
+  return pages.map(({ path }) => path);
+}
+
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  pages: getPagesPath(PAGE_MAP[`${process.env.TARO_ENV}`] ?? ALL_PAGES),
+  subPackages: SUB_PACKAGES.map(({ pages, ...item }) => ({
+    ...item,
+    pages: getPagesPath(pages),
+  })),
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-import { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
+import { PropsWithChildren } from "react";
+import { useLaunch } from "@tarojs/taro";
 
-import './app.less'
+import "./app.less";
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
-    console.log('App launched.')
-  })
+    console.log("App launched.");
+  });
 
   // children 是将要会渲染的页面
-  return children
+  return children;
 }
-  
 
-
-export default App
+export default App;
